9f60c73c097842013d097e547cd177ee28ed181c
github-admin-imatia <82836811+github-admin-imatia@users.noreply.github.com>
2024-02-16 13:30:51 +0100

    initial commit

---
+++ appmazing-frontend/src/main/ngx/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Util } from 'ontimize-web-ngx';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'o-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(
+    private router: Router
+  ) {
+    if(window['__ontimize'] !== undefined && window['__ontimize']['redirect'] !== undefined) {
+      let redirectTo = window['__ontimize']['redirect'];
+      window['__ontimize']['redirect'] = undefined;
+      this.router.navigate([redirectTo]);
+    }
+  }
+
+}
